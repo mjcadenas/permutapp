da24a4477bcb1045ce2b98502a917556043ec79f
María Jesús Cadenas Sánchez <mariajesuscadenas@gmail.com>
2020-05-04 02:38:09 +0200

    Creadas vistas (sin funcionalidad)

---
+++ permutapp/ViewControllers/LoginViewController.swift
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.swift
+//  permutapp
+//
+//  Created by Maria Jesus Cadenas Sanchez on 04/05/2020.
+//  Copyright © 2020 DlgaETSII. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func loginTapped(_ sender: Any) {
+    }
+    
+}
---
+++ permutapp/ViewControllers/SingUpViewController.swift
@@ -0,0 +1,50 @@
+//
+//  SingUpViewController.swift
+//  permutapp
+//
+//  Created by Maria Jesus Cadenas Sanchez on 04/05/2020.
+//  Copyright © 2020 DlgaETSII. All rights reserved.
+//
+
+import UIKit
+
+class SingUpViewController: UIViewController {
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var gradeTextField: UITextField!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func singUpTapped(_ sender: Any) {
+    }
+    
+    
+    
+}
